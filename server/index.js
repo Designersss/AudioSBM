3c8e23405d537985efb9c672de0a22d1e4891e90
Designersss <cheredkovden3@gmail.com>
2023-03-14 20:50:30 +0300

    Start app end fix bag

--- server/index.js
+++ server/index.js
@@ -4,16 +4,12 @@ const models = require('./models/models')
 const sequelize = require('./bd')
 const cors = require('cors')
 const router = require('./routes/index')
-
 PORT = process.env.PORT || 7000
+
+
 const app = express()
-app.use(cors())
-app.use(express.json)
 app.use('/api', router)
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'GOGO'})
-})
 const start = async () => {
     try {
         await sequelize.authenticate()
